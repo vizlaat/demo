beae1fc26d99ca73ebf32da40604854df4d57439
lgrimm1 <laszlo.grimm1@gmail.com>
2022-08-21 17:44:23 +0400

    Recursive sorting method, inital variables at call

--- src/main/java/com/example/SortingUtil.java
+++ src/main/java/com/example/SortingUtil.java
@@ -2,9 +2,31 @@ package com.example;
 
 public class SortingUtil {
     
+    private static int[] sortRecursive(int[] array, int indexLow, int indexHigh) {
+        if (array[indexLow] > array[indexHigh]) {
+            int temp = array[indexLow];
+            array[indexLow] = array[indexHigh];
+            array[indexHigh] = temp;
+        }
+        if (indexHigh == array.length - 1) {
+            indexLow++;
+            indexHigh = indexLow + 1;
+        }
+        else {
+            indexHigh++;
+        }
+        if (indexLow == array.length - 1) {
+            return array;
+        }
+        return sortRecursive(array, indexLow, indexHigh);
+    }
+
     public static int[] sortWithoutCycle(int[] somethingToSort) {
-        //megjegyzés
-        return new int[0];
+        if (somethingToSort.length < 2) {
+            return somethingToSort;
+        }
+        int indexLow = 0, indexHigh = 1;
+        return sortRecursive(somethingToSort, indexLow, indexHigh);
     }
 
 }
