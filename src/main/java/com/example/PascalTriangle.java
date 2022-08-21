da613fd3290294da892136fede5895a10e17fb96
lgrimm1 <laszlo.grimm1@gmail.com>
2022-08-21 19:01:21 +0400

    Slight correction :)

--- src/main/java/com/example/PascalTriangle.java
+++ src/main/java/com/example/PascalTriangle.java
@@ -1,11 +1,11 @@
 package com.example;
 
-public clas  PascalTriangle{
+public class  PascalTriangle{
   
   // It returns the Nth row from the Pascal Triangle
   public static int[] NthRow(int rowNum) {
     // TODO
-    return new int[];
+    return new int[0];
   }
   
 }
