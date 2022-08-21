1b0a2c8f6309d36ef4f56b176aafa476bf039846
vizlaat <vizsoro@gmail.com>
2022-08-21 15:22:11 +0100

    Create PascalTriangle.java

---
+++ src/main/java/com/example/PascalTriangle.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public clas  PascalTriangle{
+  
+  // It returns the Nth row from the Pascal Triangle
+  public static int[] NthRow(int rowNum) {
+    // TODO
+    return new int[];
+  }
+  
+}
