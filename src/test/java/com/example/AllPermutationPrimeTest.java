b2a0d0b12e19c1f979295c53477935023fef9ab9
lgrimm1 <laszlo.grimm1@gmail.com>
2022-08-25 22:48:54 +0400

    The asserts are re-written for List comparing.

--- src/test/java/com/example/AllPermutationPrimeTest.java
+++ src/test/java/com/example/AllPermutationPrimeTest.java
@@ -1,6 +1,8 @@
 package com.example;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -8,9 +10,9 @@ public class AllPermutationPrimeTest {
     
     @Test
     public void testAllCircledPermutationsArePrimes() {
-        assertArrayEquals(new Integer[0], AllPermutationPrime.allCircledPermutationsArePrimes(1).toArray());
-        assertArrayEquals(new Integer[]{2, 3}, AllPermutationPrime.allCircledPermutationsArePrimes(3).toArray());
-        assertArrayEquals(new Integer[]{2, 3, 5, 7, 11, 13, 17}, AllPermutationPrime.allCircledPermutationsArePrimes(25).toArray());
-    }
+        assertEquals(Arrays.asList(new Integer[0]), AllPermutationPrime.allCircledPermutationsArePrimes(1));
+        assertEquals(Arrays.asList(2, 3), AllPermutationPrime.allCircledPermutationsArePrimes(3));
+        assertEquals(Arrays.asList(2, 3, 5, 7, 11, 13, 17), AllPermutationPrime.allCircledPermutationsArePrimes(25));
+}
     
 }
